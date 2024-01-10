452beb874a44b33deab8180c8de02d914689bce6
Boniface <cheruiyotbony@gmail.com>
2024-01-10 19:40:46 +0300

    Assignment done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+# Calling the method
+greet_programmer()
+
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+greet('My friend')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+# Calling the method
+greet_with_default()
+
 
 def add(num1, num2):
     pass
+    return num1+num2
+add(2,4)
 
 def halve(number):
     pass
+    return float(number/2)
+halve(10)
+
+printP=halve(20)
+print(printP)
